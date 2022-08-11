6af8916132f9bcc08a474340a46e64f913bd9795
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-08-11 22:31:15 +0530

    Added : welcome msg here on master branch

---
+++ AddressBook.java
@@ -0,0 +1,11 @@
+package IOStream;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+
+		System.out.println("Assignment - 27\n");
+		System.out.println("Address Book Program using IOStream");
+	}
+
+}
